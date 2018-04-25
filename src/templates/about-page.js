b676a4c3e766b63fbb23219be431cb81d76e196b
Topher Zimmermann <topher@dandelion.org>
2018-04-25 15:36:23 +0200

    Update about-page.js

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -13,7 +13,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
             <div className="section">
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
             
-                {title} - Hackin the system (src/templates/about-page.js)
+                {title} - Hackin the system BIGTIME (src/templates/about-page.js)
               </h2>
               <PageContent className="content" content={content} />
             </div>
